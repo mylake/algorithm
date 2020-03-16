9753fdedd0095c9095d327ee592186ead8fedbdc
t-lake <lake@tripla.jp>
2020-03-17 06:41:31 +0900

    insertion_sort, selection_sort

--- geekbang/data-structure-and-algorithm/11-sort.rb
+++ geekbang/data-structure-and-algorithm/11-sort.rb
@@ -50,7 +50,49 @@ class Sort
 
       i += 1
     end
+  end
 
+  def insertion_sort(a, n)
+    return if n <= 1
+
+    i = 1
+    while i < n
+      value = a[i]
+
+      j = i - 1
+
+      while j >= 0
+        break if a[j] <= value
+
+        a[j+1] = a[j]
+
+        j -= 1
+      end
+      a[j+1] = value
+
+      i += 1
+    end
+  end
+
+  def selection_sort(a, n)
+    return if n <= 1
+
+    i = 0
+    while i < n-1
+      min_index = i
+      j = i + 1
+
+      while j < n
+        min_index = j if a[j] < a[min_index]
+        j += 1
+      end
+
+      tmp = a[i]
+      a[i] = a[min_index]
+      a[min_index] = tmp
+
+      i += 1
+    end
   end
 
 end
