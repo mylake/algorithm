3ef1c03c1de728aa9c61ef41d3fc78d5191a56ac
t-lake <lake@tripla.jp>
2020-06-03 07:32:10 +0900

    23-二叉树的深度

---
+++ get_the_offer/23-二叉树的深度/code.rb
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer}
+def max_depth(root)
+    return 0 if root.nil?
+    return 1 if root.left.nil? && root.right.nil?
+
+    return 1 + [max_depth(root.left), max_depth(root.right)].max
+end
